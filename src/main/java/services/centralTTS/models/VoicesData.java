90291bc6c53c475bad08767bafcfa9ff7059c55e
Tomislav <zorcec@gmail.com>
2018-11-10 16:33:26 +0100

    Initial commit

---
+++ src/main/java/services/centralTTS/models/VoicesData.java
@@ -0,0 +1,10 @@
+package main.java.services.centralTTS.models;
+
+import java.util.ArrayList;
+
+public class VoicesData {
+
+    public ArrayList<VoiceData> items = new ArrayList<VoiceData>();
+
+}
+  
